f9ea365b40252b89de634eabdf6284212664507b
Nikola <milje18@gmail.com>
2019-06-02 14:47:10 +0200

    basic layout with header,footer and vue material component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+  <comp-header/>
+    <div>
+      hello
+    </div>
+  <comp-footer/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+import compHeader from './components/Header-footer/Header.vue';
+import compFooter from './components/Header-footer/Footer.vue';
+
+
+export default{
+  components:{
+    compHeader,
+    compFooter
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/style.css';
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { MdButton, MdDrawer } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdButton)
+Vue.use(MdDrawer)
+
+
 Vue.config.productionTip = false
 
 new Vue({
+  el:"#app",
   render: h => h(App),
-}).$mount('#app')
+})
