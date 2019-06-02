7b8f3cf1ce7784f6f70c1ab918780c05b5e059a5
Nikola <milje18@gmail.com>
2019-06-02 15:58:33 +0200

    menu buttons added

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <comp-header/>
     <div>
       <comp-featured/>
+      <comp-menu/>
     </div>
   <comp-footer/>
 </div>
@@ -13,12 +14,14 @@
 import compHeader from './components/Header-footer/Header.vue';
 import compFooter from './components/Header-footer/Footer.vue';
 import compFeatured from './components/Featured/Index.vue';
+import compMenu from './components/Menu/Index.vue';
 
 export default{
   components:{
     compHeader,
     compFooter,
-    compFeatured
+    compFeatured,
+    compMenu
   }
 }
 </script>
