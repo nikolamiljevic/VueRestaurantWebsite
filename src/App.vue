80ee6238798527c47abe54c3d906143c1d5703ec
Nikola <milje18@gmail.com>
2019-06-04 12:27:09 +0200

    velocity js added for animations of the footer elements

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <div>
       <comp-featured/>
       <comp-menu/>
+      <comp-call-us/>
     </div>
   <comp-footer/>
 </div>
@@ -15,13 +16,15 @@ import compHeader from './components/Header-footer/Header.vue';
 import compFooter from './components/Header-footer/Footer.vue';
 import compFeatured from './components/Featured/Index.vue';
 import compMenu from './components/Menu/Index.vue';
+import compCallUs from './components/CallUs/Index.vue'
 
 export default{
   components:{
     compHeader,
     compFooter,
     compFeatured,
-    compMenu
+    compMenu,
+    compCallUs
   }
 }
 </script>
