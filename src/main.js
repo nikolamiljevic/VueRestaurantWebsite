aef0543424d4521233129d99c78ab3a6fd8a6ff0
Nikola <milje18@gmail.com>
2019-06-04 11:26:38 +0200

    vue scroll reveal added on page menu buttons and div with products

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import VueScrollReveal from 'vue-scroll-reveal'
+
 
 import { MdButton, MdDrawer, MdCard } from 'vue-material/dist/components'
 import 'vue-material/dist/vue-material.min.css'
@@ -11,6 +13,11 @@ Vue.use(MdButton)
 Vue.use(MdDrawer)
 Vue.use(VueResource)
 
+Vue.use(VueScrollReveal,{
+  duration:800,
+  scale:1,
+  distance:'50px'
+});
 
 Vue.http.options.root = 'https://domnoopizza.firebaseio.com/'
 
